408c824477ecde432149eed29fd8e45a0576f7f9
yoichimichael <yoichi.michael.nagano@gmail.com>
2020-05-13 07:31:53 -0700

    added arrow press list entries

--- index.js
+++ index.js
@@ -3,4 +3,26 @@ document.addEventListener("DOMContentLoaded", function(){
   renderBot(currentPosition)
 
   // ADD CODE HERE!
+
+  document.addEventListener("keydown", function(e) {
+    e.preventDefault()
+    let movesList = document.getElementById("moves-container");
+    let move = document.createElement("li");
+
+    if (e.key === "ArrowLeft") {
+      move.textContent = "Left"
+      movesList.appendChild(move);      
+    } else if (e.key === "ArrowRight") {
+      move.textContent = "Right"
+      movesList.appendChild(move);
+    } else if (e.key === "ArrowUp") {
+      move.textContent = "Up"
+      movesList.appendChild(move);
+    } else if (e.key === "ArrowDown") {
+      move.textContent = "Down"
+      movesList.appendChild(move);
+    };
+
+  });
+
 })
